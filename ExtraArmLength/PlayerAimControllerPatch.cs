4a2e379950fb8a0988bbe883e5f954cdf9e6e67c
Luke Vo <lukevo93@outlook.com>
2024-07-07 00:01:56 +0700

    Added ExtraArmLength.

---
+++ ExtraArmLength/PlayerAimControllerPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using SpaceCraft;
+using UnityEngine;
+
+namespace ExtraArmLength
+{
+
+    partial class Plugin
+    {
+
+        [HarmonyPatch(typeof(PlayerAimController), "SetLayerMaskAndDistance")]
+        public class PlayerAimControllerPatch
+        {
+
+            static void Postfix(ref float ____distanceHitLimit)
+            {
+                ____distanceHitLimit = distance.Value;
+            }
+
+        }
+
+    }
+
+}
---
+++ ExtraArmLength/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using CommonPlugin;
+
+namespace ExtraArmLength
+{
+
+    [BepInPlugin("com.lukev.extraarmlength", "Extra Arm Length", "1.0.0")]
+    public partial class Plugin : BasePlugin
+    {
+        static ConfigEntry<float> distance;
+
+
+        protected override void OnAwake()
+        {
+            distance = Config.Bind("General", "TotalLength", 15f, "Distance you can interact with things");
+        }
+    }
+}
